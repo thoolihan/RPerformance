3a9b8a354cdeafb3bf3ae94e0b092963177c9c30
Tim Hoolihan <tim@hoolihan.net>
2018-01-09 18:35:54 -0500

    Basic ff demo

--- 06-ff.R
+++ 06-ff.R
@@ -1,2 +1,20 @@
 library(ff)
 library(ffbase)
+library(pryr)
+
+fh <- file("data/activity/Phones_accelerometer.csv", open = "rt")
+
+# run only the first time
+#df <- read.csv.ffdf(file = "data/activity/Phones_accelerometer.csv")
+#ffsave(df, file = "phones")
+
+ffload(file = "phones", overwrite = TRUE)
+
+class(df)
+object_size(df)
+names(df)
+nrow(df)
+
+unique(df$Model)
+mean(df$x)
+mean(df$y)
