1fa242a68d242b6e52f6d276d3c1e481d807195d
Tim Hoolihan <tim@hoolihan.net>
2018-01-09 19:18:25 -0500

    parallel example ready

--- 07-parallel.R
+++ 07-parallel.R
@@ -1,1 +1,19 @@
 library(parallel)
+
+ncores <- detectCores()
+ncores
+
+cl <- makeCluster(ncores - 1)
+
+store_by_bits <- parLapply(cl, 1:16, function(x) 2 ^ x)
+store_by_bits
+
+library(nnet)
+
+clusterEvalQ(cl, {library(nnet)})
+
+models <- parLapply(cl, 1:7, function(neurons) 
+  nnet(Species ~ ., data = iris, size = neurons))
+
+models
+stopCluster(cl)
